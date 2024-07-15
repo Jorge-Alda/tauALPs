ba09cb671a050455f68777261565b5bba897f3cb
Jorge Alda <jorge.alda@pd.infn.it>
2024-07-15 10:50:08 +0200

    Same y-axis for all plots

--- ALP/Plots/moneyplot_ALPtau.py
+++ ALP/Plots/moneyplot_ALPtau.py
@@ -8,20 +8,22 @@ from astro_cosmo import sn_gamma_tau, sn_nu_tau, sn_pvo_tau, sne_tau, gw_fermi_t
 BelleII_g2tau_tau.textpos = (1.3,13)
 BelleII_g2tau_tau.name = r'$(g-2)_\tau$ (Belle II)'
 gammainv.color = 'brown'
-gammainv.textpos = (1.2e-3, 2.2e-2)
-gammainv_proj.textpos = (1.2e-3, 1.85e-3)
+gammainv.textpos = (1.2e-3, 2e-2)
+gammainv_proj.textpos = (1.2e-3, 7.3e-4)
 gammainv_proj.color = 'brown'
 eetaugamma.color = 'red'
 eetaugamma_proj.color = 'red'
-eetaugamma.textpos = (1, 80)
+eetaugamma.textpos = (1, 40)
+eetaugamma_proj.textpos = (1, 0.6)
 ee3gamma.color = 'lightsalmon'
-ee3gamma.textpos = (0.2, 0.1)
+ee3gamma.textpos = (0.2, 0.3)
 ee3gamma_proj.color = 'lightsalmon'
 ee3gamma_proj.textpos = (0.28, 1.8e-2)
 BelleII_g2tau_tau.color = 'purple'
 sne_tau.textpos = (0.5, 1.5e-1)
+sn_nu_tau.textpos = (0.35, 0.4)
 ATLAS_g2tau_tau.color = 'purple'
-ATLAS_g2tau_tau.textpos = (1.2e-3, ATLAS_g2tau_tau.textpos[1])
+ATLAS_g2tau_tau.textpos = (1.2e-3, 170)
 
 if __name__ == '__main__':
-    plots.make_plot_tau([sn_gamma_tau, sn_nu_tau, sne_tau, gw_fireball_tau, BelleII_g2tau_tau, ATLAS_g2tau_tau, gammainv, ee3gamma, eetaugamma, gammainv_proj, ee3gamma_proj, eetaugamma_proj], 'moneyplot_ALPtau.pdf', limy=(1e-6, 2), limx=(1e-3, 10))
+    plots.make_plot_tau([sn_gamma_tau, sn_nu_tau, sne_tau, gw_fireball_tau, BelleII_g2tau_tau, ATLAS_g2tau_tau, gammainv, ee3gamma, eetaugamma, gammainv_proj, ee3gamma_proj, eetaugamma_proj], 'moneyplot_ALPtau.pdf', limy=(1e-9, 10), limx=(1e-3, 10))
--- PseudoScalar/Plots/moneyplot_PStau.py
+++ PseudoScalar/Plots/moneyplot_PStau.py
@@ -30,4 +30,4 @@ if __name__ == '__main__':
         BelleII_g2tau, ATLAS_g2tau,
         eetaugamma, eetaugamma_proj,
         gammainv, ee3gamma, gammainv_proj, ee3gamma_proj
-        ], 'moneyplot_PStau.pdf', limy=(1e-9, 2), limx=(1e-3, 10))
+        ], 'moneyplot_PStau.pdf', limy=(1e-9, 10), limx=(1e-3, 10))
